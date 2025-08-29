a28d4316012f5f7bebb14ebb3fff9a256f2c1f57
patilsayli1109 <patilsayli1109@gmail.com>
2025-08-29 04:21:10 +0000

    second commit

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Video from '../home/Video'
 
 const HomeHeroText = () => {
   return (
-    <div>HomeHeroText</div>
+    <div className='font-[font1] text-center pt-5'>
+      <div className='text-[9.5vw] leading-[8vw] uppercase'>The spark</div>
+    </div>
   )
 }
 
