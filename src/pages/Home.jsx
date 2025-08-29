f6bcf1c7ee06abd90580319de65e23c8d254eecc
patilsayli1109 <patilsayli1109@gmail.com>
2025-08-29 03:24:55 +0000

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Video from '../components/home/Video'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='w-screen h-screen bg-red-500'>
+            <Video/>
+        </div>
+    </div>
+  )
+}
+
+export default Home
