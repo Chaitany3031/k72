4933bd9d0491443d853d31c435a803cd5eff63d5
patilsayli1109 <patilsayli1109@gmail.com>
2025-08-29 05:06:05 +0000

    committed

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -6,7 +6,7 @@ const HomeHeroText = () => {
     <div className='font-[font1] text-center pt-5'>
       <div className='text-[9.5vw] leading-[8vw] justify-center flex items-center uppercase'>The spark</div>
       <div className='text-[9.5vw] leading-[8vw] justify-center flex items-center uppercase'>who</div>
-      <div className='h-[7vw] rounded-full overflow-hidden -mt-5'><Video/></div>
+      <div className='h-[7vw] rounded-full overflow-hidden -mt-3'><Video/></div>
       <div className='text-[9.5vw] leading-[9.5vw] justify-center flex items-center uppercase'>Generates</div>
       <div className='text-[9.5vw] leading-[8vw] justify-center flex items-center uppercase'>the creativity</div>
     </div>
--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -6,7 +6,7 @@ const Video = () => {
   return (
    <div>
      <div className='w-full h-full'>
-      <video className='w-full h-full object-cover fixed' autoPlay muted loop src="https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1756437145~exp=1756440745~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=1aa9b049603fe2dd4599a5d90af10e51ccc624cefccb8062d0f564a6fba31d91&r=dXMtd2VzdDE%3D"></video>
+      <video className='w-full h-full object-cover fixed' autoPlay muted loop src="../../../public/video.mp4"></video>
     </div>
     <div className='h-screen w-screen relative flex flex-col'>
       <HomeHeroText/>
