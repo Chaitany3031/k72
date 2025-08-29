04615a0434f825b7d2ec825476af3a3752f98de9
patilsayli1109 <patilsayli1109@gmail.com>
2025-08-29 04:25:22 +0000

    third commit

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -4,7 +4,11 @@ import Video from '../home/Video'
 const HomeHeroText = () => {
   return (
     <div className='font-[font1] text-center pt-5'>
-      <div className='text-[9.5vw] leading-[8vw] uppercase'>The spark</div>
+      <div className='text-[9.5vw] leading-[8vw] justify-center flex items-center uppercase'>The spark</div>
+      <div className='text-[9.5vw] leading-[8vw] justify-center flex items-center uppercase'>who</div>
+      <div className='h-[7vw] rounded-full overflow-hidden -mt-5'><Video/></div>
+      <div className='text-[9.5vw] leading-[9.5vw] justify-center flex items-center uppercase'>Generates</div>
+      <div className='text-[9.5vw] leading-[8vw] justify-center flex items-center uppercase'>the creativity</div>
     </div>
   )
 }
